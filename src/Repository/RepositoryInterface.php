ba318d4db6a5d65514e73b8826c052a2b7fb965a
JoeBengalen <joebengalen@gmail.com>
2015-03-12 17:31:16 +0100

    Add some methods to the repository interface

--- src/Repository/RepositoryInterface.php
+++ src/Repository/RepositoryInterface.php
@@ -2,11 +2,35 @@
 
 namespace JoeBengalen\Cache\Repository;
 
+use Psr\Cache\CacheItemInterface;
+
+// api: contains, fetch, persist, delete and clear. And xxAll for multiple items at once, except for the latter.
 interface RepositoryInterface
 {
-    public function fetch($key); // key:string, return:Item|null
-    public function clear(); // return:boolean
-    public function persist($keys); // keys:string|string[]
-    public function delete($keys); // keys:string|string[]
+    // key: string, return:boolean
+    public function contains($key);
+    
+    // key: string[], return: array as [key => boolean]
+    public function containsAll(array $keys);
+    
+    // key: string, return: Item|null
+    public function fetch($key);
+    
+    // key: string[], return: array as [key => Item|null]
+    public function fetchAll(array $keys);
+    
+    // return: boolean
+    public function persist(CacheItemInterface $item);
+    
+    // item: Item[], return: boolean
+    public function persistAll(array $items);
+    
+    // key: string, return: boolean
+    public function delete($key);
+    
+    // keys: string[], return: boolean
+    public function deleteAll(array $keys);
+    
+    // return: boolean
+    public function clear();
 }
-
